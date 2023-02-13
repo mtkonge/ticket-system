59015ef3d1fe777b509430f98fac4bcb13d67e68
ReimarPB <reimarpb@gmail.com>
2023-02-13 14:35:29 +0100

    Frontend: Implement components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,75 @@
-function main() {
-    console.log("hElLo WoRlD")
+function generateId() {
+	return "a" + Math.random().toString().substr(2);
 }
+
+function select<T extends HTMLElement>(id: string): T {
+	const result = document.querySelector<T>("#" + id);
+	if (!result) {
+		throw new Error("Could not find element from id: " + id);
+	}
+	return result;
+}
+
+abstract class Component {
+
+	public abstract render(): string;
+
+	public children(): Component[] { return [] }
+
+	public hydrate(update: () => void) {}
+
+}
+
+class MainPage extends Component {
+
+	private state = { value: "test" };
+	private textInput = new TextInput(this.state);
+
+	render() {
+		return /*html*/`
+			<h1>${this.state.value}</h1>
+			${this.textInput.render()}
+		`;
+	}
+
+	children() {
+		return [ this.textInput ];
+	}
+
+}
+
+class TextInput extends Component {
+
+	private buttonId = generateId();
+	private inputId = generateId();
+
+	constructor(private state: { value: string }) {
+		super();	
+	}
+
+	render() {
+		return `<input id="${this.inputId}"><button id="${this.buttonId}">hello</button>`;
+	}
+
+	hydrate(update: () => void) {
+		document.getElementById(this.buttonId)!.addEventListener("click", event => {
+			this.state.value = document.querySelector<HTMLInputElement>("#" + this.inputId)!.value;
+			update();
+		});
+	}
+}
+
+function hydrateChildren(component: Component, update: () => void) {
+	component.hydrate(update);
+	component.children().forEach(child => hydrateChildren(child, update));
+}
+
+function rerenderAndHydrate(main: Component) {
+	document.body.innerHTML = main.render();
+	requestAnimationFrame(() => {
+		hydrateChildren(main, () => rerenderAndHydrate(main));
+	});
+}
+const mainPage = new MainPage();
+rerenderAndHydrate(mainPage);
+
