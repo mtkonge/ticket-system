387026a372639154067371cc8d8efe73f0aba794
ReimarPB <reimarpb@gmail.com>
2023-02-14 10:36:51 +0100

    Backend: Add support for static files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,12 +3,18 @@ mod db;
 use actix_cors::Cors;
 use actix_web::{
     get,
-    http::header::ContentType,
+    body::BoxBody,
+    http::StatusCode,
+    http::header::{ContentType},
     web::{self, Data},
-    App, HttpRequest, HttpResponse, HttpServer, Responder,
+    App, HttpRequest, HttpResponse, HttpResponseBuilder, HttpServer, Responder,
 };
 use futures::lock::Mutex;
 use serde::Serialize;
+use std::path::Path;
+use std::fs;
+use std::fs::metadata;
+use std::io;
 
 use crate::db::TicketDb;
 
@@ -30,6 +36,43 @@ async fn increment(db: web::Data<Mutex<TicketDb>>, _req: HttpRequest) -> impl Re
         })
 }
 
+#[get("/{filename:.*}")]
+async fn static_files(req: HttpRequest) -> impl Responder {
+    let mut path = req.match_info().query("filename").parse().unwrap();
+    path = Path::new("../frontend").join(path);
+
+    match metadata(path.clone()) {
+        Ok(md) => {
+            if md.is_dir() {
+                path = path.join("index.html");
+            }
+        },
+        Err(err) =>
+            return if err.kind() == io::ErrorKind::NotFound {
+                http_default_response(404)
+            } else {
+                http_default_response(500)
+            },
+    }
+
+    return match fs::read_to_string(path) {
+        Ok(content) =>
+            HttpResponse::Ok()
+                .body(content),
+        Err(err) =>
+            if err.kind() == io::ErrorKind::NotFound {
+                http_default_response(404)
+            } else {
+                http_default_response(500)
+            },
+    };
+}
+
+fn http_default_response(status: u16) -> HttpResponse<BoxBody> {
+    HttpResponseBuilder::new(StatusCode::from_u16(status).unwrap())
+        .body(format!("<body bgcolor='000'><center><img src='https://http.cat/{0}' alt='{0}'></center></body>", status))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let db = TicketDb::new();
@@ -39,6 +82,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(Data::from(db.clone()))
             .wrap(Cors::permissive())
             .service(increment)
+            .service(static_files)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
