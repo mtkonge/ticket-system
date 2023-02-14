5f7ce7dadf517da18e2eb8844a10d3850696b83a
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-02-14 14:35:02 +0100

    /assets should look in /assets

--- backend/src/routes/load_assets.rs
+++ backend/src/routes/load_assets.rs
@@ -4,7 +4,7 @@ use std::{fs, io, path::Path};
 
 #[get("/assets/{filename:.*}")]
 pub async fn load_assets(_: HttpRequest, filename: web::Path<String>) -> impl Responder {
-    let path = Path::new("../frontend").join(Path::new(&*filename));
+    let path = Path::new("../frontend/assets").join(Path::new(&*filename));
 
     match fs::read_to_string(path) {
         Ok(content) => HttpResponse::Ok().body(content),
