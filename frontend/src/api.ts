7014cfeb294a3d65a176d46c72224fad6b37744e
SimonFJ20 <simonfromjakobsen@gmail.com>
2023-02-15 12:25:21 +0100

    add login and edit role

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,27 @@
 
+export type EditUserRoleRequest = {
+	token: string,
+	user_id: number,
+	role: "Consumer" | "LevelOne" | "LevelTwo" | "Admin",
+}
+
+export type EditUserRoleResponse = {
+	ok: boolean,
+	msg: string,
+}
+
+export async function editUserRole(request: EditUserRoleRequest): Promise<EditUserRoleResponse> {
+	const response = await fetch("/api/user/edit_role", {
+		method: "POST",
+		headers: new Headers({ "Content-Type": "application/json" }),
+		body: JSON.stringify(request),
+	});
+	return {
+		...await response.json(),
+		ok: response.ok,
+	};
+}
+
 export type RegisterUserRequest = {
 	username: string,
 	password: string,
@@ -17,6 +40,31 @@ export async function registerUser(request: RegisterUserRequest): Promise<Regist
 	});
 	return {
 		...await response.json(),
+		ok: response.ok,
+	};
+}
+
+export type LoginRequest = {
+	username: string,
+	password: string,
+};
+
+export type LoginResponse = {
+	ok: boolean,
+	msg: string,
+	token?: string,
+}
+
+export async function loginUser(request: LoginRequest): Promise<LoginResponse> {
+	const response = await fetch("/api/user/login", {
+		method: "POST",
+		headers: new Headers({ "Content-Type": "application/json" }),
+		body: JSON.stringify(request),
+	});
+	const data = await response.json();
+	return {
+		...data,
+		token: data["token"] ?? data["session"] ?? undefined,
 		ok: response.ok,
 	};
 }
