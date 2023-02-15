7d80bed3570051ad46a3c30fb67e453fc5a5f8f6
SimonFJ20 <simonfromjakobsen@gmail.com>
2023-02-15 19:48:02 +0100

    use context in Router.ts

--- frontend/src/Router.ts
+++ frontend/src/Router.ts
@@ -8,6 +8,8 @@ import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { AdminPanel } from "./pages/AdminPanel";
 import { CreateTicket } from "./pages/CreateTicket";
+import { TicketEditor } from "./pages/TicketEditor";
+import { Context } from "./Context";
 
 export class RouterPage implements Component {
     private indexPage = new Index();
@@ -17,55 +19,60 @@ export class RouterPage implements Component {
     private registerPage: Register;
     private adminPanelPage: AdminPanel;
     private createTicketPage: CreateTicket;
+    private ticketEditorPage: TicketEditor;
 
     public constructor(
-        private router: Router,
-        private session: ByRef<Session | null>,
+        private context: Context,
     ) {
-        this.customerPage = new Customer(this.router);
-        this.loginPage = new Login(this.router, this.session);
-        this.registerPage = new Register(this.router, this.session);
-        this.adminPanelPage = new AdminPanel(this.router, this.session);
-        this.createTicketPage = new CreateTicket(this.router, this.session);
+        this.customerPage = new Customer(this.context);
+        this.loginPage = new Login(this.context);
+        this.registerPage = new Register(this.context);
+        this.adminPanelPage = new AdminPanel(this.context);
+        this.createTicketPage = new CreateTicket(this.context);
+        this.ticketEditorPage = new TicketEditor(this.context);
     }
 
     public render() {
-        if (this.router.route() == "/") return this.indexPage.render();
-        else if (this.router.route() == "/customer")
+        if (this.context.router.route() == "/") return this.indexPage.render();
+        else if (this.context.router.route() == "/customer")
             return this.customerPage.render();
-        else if (this.router.route() == "/supporter")
+        else if (this.context.router.route() == "/supporter")
             return this.supporterPage.render();
-        else if (this.router.route() == "/login")
+        else if (this.context.router.route() == "/login")
             return this.loginPage.render();
-        else if (this.router.route() == "/register")
+        else if (this.context.router.route() == "/register")
             return this.registerPage.render();
-        else if (this.router.route() == "/admin_panel")
+        else if (this.context.router.route() == "/admin_panel")
             return this.adminPanelPage.render();
-        else if (this.router.route() == "/create_ticket")
+        else if (this.context.router.route() == "/create_ticket")
             return this.createTicketPage.render();
+        else if (this.context.router.route() == "/ticket_editor")
+            return this.ticketEditorPage.render();
         return "<img src='https://http.cat/404.jpg'>";
     }
 
     public children() {
-        if (this.router.route() == "/") return [this.indexPage];
-        else if (this.router.route() == "/customer")
+        if (this.context.router.route() == "/") return [this.indexPage];
+        else if (this.context.router.route() == "/customer")
             return [this.customerPage];
-        else if (this.router.route() == "/supporter")
+        else if (this.context.router.route() == "/supporter")
             return [this.supporterPage];
-        else if (this.router.route() == "/login") return [this.loginPage];
-        else if (this.router.route() == "/register")
+        else if (this.context.router.route() == "/login") return [this.loginPage];
+        else if (this.context.router.route() == "/register")
             return [this.registerPage];
-        else if (this.router.route() == "/admin_panel")
+        else if (this.context.router.route() == "/admin_panel")
             return [this.adminPanelPage];
-        else if (this.router.route() == "/create_ticket")
+        else if (this.context.router.route() == "/create_ticket")
             return [this.createTicketPage];
+        else if (this.context.router.route() == "/ticket_editor")
+            return [this.ticketEditorPage];
         else return [];
     }
 
     public hydrate(update: () => void) {
         // not using addEventListener to clear previous
         window.onpopstate = () => {
-            this.router.updateRouteToUrl();
+            this.context.router.updateRouteToUrl();
             update();
         };
     }
